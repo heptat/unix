7f54a5eabfee643640bfe7eb4e0cbc07f0769167
heptat <heptat@gmail.com>
2013-03-13 12:54:59 +0000

    minor update

--- file_copy2/file_copy.c
+++ file_copy2/file_copy.c
@@ -3,7 +3,7 @@
 int main() {
   int c = 0;
   // this works but it's not really acceptable behaviour and you can't use
-  // fileno with c99
+  // fileno with c99:
   // if (isatty(fileno(stdin)) != 1) {
   while ((c = fgetc(stdin)) != EOF) {
     fputc(c, stdout);
